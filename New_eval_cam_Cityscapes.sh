5ceeb0ad237c64da569d5918226c038ad446a4f9
letitiabanana <100341866+letitiabanana@users.noreply.github.com>
2024-01-28 23:18:55 +0800

    Update New_eval_cam_Cityscapes.sh

--- New_eval_cam_Cityscapes.sh
+++ New_eval_cam_Cityscapes.sh
@@ -1,6 +1,6 @@
 python New_eval_cam_Cityscapes.py \
 --cam_out_dir ./New_Cbatch_Eval_test_ddp_0126_768_flickrfinetune_zeroshot_halvingdrop_Cityscapes \
-  --save_path ./Cityscapes/OPT_Cityscapes_IOU_layer8head9_dropiter5_768_flickrfinetune_zeroshot_attthresh15_blurcrf \
+--save_path ./Cityscapes/OPT_Cityscapes_IOU_layer8head9_dropiter5_768_flickrfinetune_zeroshot_attthresh15_blurcrf \
 --cam_threshold 0.15 --drop_patch_eval halving \
 --img_size 768 \
 --del_patch_num sort_thresh005 --drop_iter 5 \
