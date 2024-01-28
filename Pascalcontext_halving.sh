9d8a96378260646c1daeeec6f05f1d03e8c2ffdd
letitiabanana <100341866+letitiabanana@users.noreply.github.com>
2024-01-28 22:17:04 +0800

    Update Pascalcontext_halving.sh

--- Pascalcontext_halving.sh
+++ Pascalcontext_halving.sh
@@ -2,11 +2,11 @@
 #
 #do
 #  echo "head" $head
-CUDA_VISIBLE_DEVICES=2 python pnp_get_attention_textloc_weaklysupervised_search_PSC.py \
---save_path Cbatch_Eval_test_ddp_1111_$320_flickrfinetune_zeroshot_halvingdrop_PSC \
---master_port 22990 --gen_multiplecap_withpnpvqa label --world_size 1 \
+CUDA_VISIBLE_DEVICES=1,2 python pnp_get_attention_textloc_weaklysupervised_search_PSC.py \
+--save_path Cbatch_Eval_test_ddp_1111_768_flickrfinetune_zeroshot_halvingdrop_PSC \
+--master_port 22990 --gen_multiplecap_withpnpvqa label --world_size 2 \
 --del_patch_num sort_thresh005 \
---img_size 320 \
+--img_size 768 \
 --batch_size 28 \
 --max_att_block_num 8 --drop_iter 5 --prune_att_head 9 --sort_threshold 0.05
 
