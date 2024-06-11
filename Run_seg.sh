eeac03a5d2f9b539dfc03bd2e224bc4c1edcfa3d
letitiabanana <100341866+letitiabanana@users.noreply.github.com>
2024-06-11 17:13:56 +0800

    Update and rename New_eval_cam_PSC.sh to Run_seg.sh

---
+++ Run_seg.sh
@@ -0,0 +1,11 @@
+python PnP_OVSS_0514_updated_segmentation.py \
+  --home_dir \home\letitiabanana\LAVIS
+  --save_path BLIP_0602_336_ADE20K_segmentation\
+  --master_port 29790 --gen_multiplecap_withpnpvqa label --world_size 1 \
+  --img_size 336 \
+  --del_patch_num sort_thresh005 \
+  --batch_size 35 \
+  --max_att_block_num 8 --drop_iter 4 --prune_att_head 9 --sort_threshold 0.05 \
+  --threshold 0.15 \
+  --postprocess blur+crf \
+  --data_type ade20k
