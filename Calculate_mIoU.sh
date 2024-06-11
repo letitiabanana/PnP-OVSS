fb42d0cfecbf5ead49eea9ae9397e5abe9f04312
letitiabanana <100341866+letitiabanana@users.noreply.github.com>
2024-06-11 17:34:53 +0800

    Add files via upload

---
+++ Calculate_mIoU.sh
@@ -0,0 +1,6 @@
+
+python Calculate_mIoU.py \
+--save_path BLIP_0602_336_ADE20K_segmentation \
+--data_type ade20k
+
+#CUDA_VISIBLE_DEVICES=2,3 nohup > > sort_thresh005_320_max8att9_wobackground_flikrfinetune_zeroshot.out --ensemble_blocks saveall
